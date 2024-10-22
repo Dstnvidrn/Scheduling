c76dcaea0b6c1dc416f3c2fca4d24a984b334c16
Dustin Vidrine <Dstnvidrn@gmail.com>
2024-10-22 12:32:33 -0500

    Added initial project for scheduling application

---
+++ Scheduling/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Scheduling
+{
+    partial class formLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUserLocation = new System.Windows.Forms.Label();
+            this.txtbxUsername = new System.Windows.Forms.TextBox();
+            this.txtbxPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelUserLocation
+            // 
+            this.labelUserLocation.AutoSize = true;
+            this.labelUserLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelUserLocation.Location = new System.Drawing.Point(740, 9);
+            this.labelUserLocation.Name = "labelUserLocation";
+            this.labelUserLocation.Size = new System.Drawing.Size(48, 20);
+            this.labelUserLocation.TabIndex = 0;
+            this.labelUserLocation.Text = "lorem";
+            // 
+            // txtbxUsername
+            // 
+            this.txtbxUsername.Location = new System.Drawing.Point(272, 240);
+            this.txtbxUsername.Name = "txtbxUsername";
+            this.txtbxUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtbxUsername.TabIndex = 1;
+            // 
+            // txtbxPassword
+            // 
+            this.txtbxPassword.Location = new System.Drawing.Point(272, 287);
+            this.txtbxPassword.Name = "txtbxPassword";
+            this.txtbxPassword.PasswordChar = '●';
+            this.txtbxPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtbxPassword.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(272, 326);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // formLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtbxPassword);
+            this.Controls.Add(this.txtbxUsername);
+            this.Controls.Add(this.labelUserLocation);
+            this.Name = "formLogin";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.formLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUserLocation;
+        private System.Windows.Forms.TextBox txtbxUsername;
+        private System.Windows.Forms.TextBox txtbxPassword;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
