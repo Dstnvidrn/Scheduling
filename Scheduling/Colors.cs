176f41c4806076865187bdc44375ae0c8ee7c7db
Dustin Vidrine <Dstnvidrn@gmail.com>
2024-10-23 22:21:05 -0500

    Added Colors Class for storing reusable colors

---
+++ Scheduling/Colors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduling
+{
+    public static class Colors
+    {
+        public static string PrimaryColor { get; } = "#F5F5F5";
+        public static string BaseColor { get; } = "#3F4857";
+        public static string CtaColor { get; } = "#FF6F61";
+        public static string NeutralLightColor { get; } = "#F5F5F5";
+        public static string SecondaryColor { get; } = "#6FB1FC";
+        public static string NeutalDarkColor { get; } = "#2E3440";
+    }
+}
