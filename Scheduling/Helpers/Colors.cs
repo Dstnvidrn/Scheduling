4d5c5d733dae8236b74da9fac4206235f1843868
Dustin Vidrine <Dstnvidrn@gmail.com>
2024-11-04 12:57:56 -0600

    Updated color hex value for gray after assinging to wrong variable

--- Scheduling/Helpers/Colors.cs
+++ Scheduling/Helpers/Colors.cs
@@ -10,9 +10,9 @@ namespace Scheduling
     {
         public static string PrimaryColor { get; } = "#F5F5F5";
         public static string BaseColor { get; } = "#3F4857";
-        public static string CtaColor { get; } = "#A9A9A9";
+        public static string CtaColor { get; } = "#FF6F61";
         public static string NeutralLightColor { get; } = "#F5F5F5";
         public static string SecondaryColor { get; } = "#6FB1FC";
-        public static string NeutalDarkColor { get; } = "#2E3440";
+        public static string NeutalDarkColor { get; } = "#A9A9A9";
     }
 }
