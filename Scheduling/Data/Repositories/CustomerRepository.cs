2e8da11109387d5bf775b9b0088b0d414f4e4f6b
Dustin Vidrine <Dstnvidrn@gmail.com>
2024-11-04 21:59:54 -0600

    Added CustomerRepository class and added properties to Address

---
+++ Scheduling/Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduling.Data.Repositories
+{
+    public class CustomerRepository
+    {
+        public CustomerRepository()
+        {
+            
+        }
+        public CustomerRepository GetCustomerWith
+    }
+}
--- Scheduling/Models/Address.cs
+++ Scheduling/Models/Address.cs
@@ -8,5 +8,17 @@ namespace Scheduling.Models
 {
     public class Address
     {
+        public int AddressId { get; set; }
+        public string Street { get; set; }
+        public string Street2 { get; set; }
+        public City City { get; set; }
+        public Country Country { get; set; }
+        public string PostalCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public User CreatedBy { get; set; }
+        public DateTime LastUpdate {  get; set; }
+        public User LastUpdatedBy { get; set; }
+
     }
 }
