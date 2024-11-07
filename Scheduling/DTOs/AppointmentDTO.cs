026d901c5583529c3dee9de0b75770252a7e122d
Dustin Vidrine <Dstnvidrn@gmail.com>
2024-11-07 00:17:48 -0600

    Created DTO directory and added AppointmentDTO

---
+++ Scheduling/DTOs/AppointmentDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduling.DTOs
+{
+    public class AppointmentDTO
+    {
+        public string CustomerName { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Location { get; set; }
+        public string Contact { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public string UpdatedBy { get; set;}
+    }
+}
