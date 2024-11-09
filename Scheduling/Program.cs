6f5e602b5a5940d4ec4c1543b4c3c16d57abb75e
Dustin Vidrine <Dstnvidrn@gmail.com>
2024-11-08 22:06:50 -0600

    Refactored main program to bootstrap user services and database connection for login form

--- Scheduling/Program.cs
+++ Scheduling/Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Scheduling.Data;
+using Scheduling.Data.Repositories;
+using Scheduling.Helpers;
+using Scheduling.Interfaces;
+using Scheduling.Services;
+using Scheduling.Services.Mappers;
+using System;
+using System.Configuration;
 using System.Windows.Forms;
 
 namespace Scheduling
@@ -14,9 +18,19 @@ namespace Scheduling
         [STAThread]
         static void Main()
         {
+            // Database connection
+            var _connectionString = ConfigurationManager.AppSettings["MySqlDatabaseConnection"];
+            var _connectionFactory = new DatabaseConnectionFactory(_connectionString);
+            var _dataBaseHelper = new DatabaseHelper(_connectionFactory);
+
+            // Repository Setup
+            IUserRepository _userRepository = new UserRepository(_dataBaseHelper);
+            IUserMapper _userMapper = new UserMapper();
+            // Service Setup
+            UserService userService = new UserService(_userRepository, _userMapper);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new LoginForm(_));
         }
     }
 }
