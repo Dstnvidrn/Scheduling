861422cee114aed545a272d6a59a5f23786eab5f
Dustin Vidrine <Dstnvidrn@gmail.com>
2024-11-08 22:00:31 -0600

    Added DTO classes for Data retrieval to UI controls

---
+++ Scheduling/DTOs/CustomerDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduling.DTOs
+{
+    public class CustomerDTO
+    {
+        public string Name { get; set; }
+        public int AddressId { get; set; }
+        public bool Active { get; set; } = true;
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public string CreatedBy { get; set; }
+        public DateTime LastUpdate { get; set; } = DateTime.Now;
+        public string LastUpdatedBy { get; set; }
+
+    }
+}
